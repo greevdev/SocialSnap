fec24b046dc8ccd6f5415252e87d6bdd953943dd
greevdev <greev1@protonmail.com>
2023-08-06 01:14:20 +0300

    Start implementation of Profile page, Create new logo

--- SocialSnap/Site/urls.py
+++ SocialSnap/Site/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
-from .views import landing_page, dashboard_page
+from .views import landing_page, dashboard_page, UserProfileView, my_profile
 
 urlpatterns = [
     path('', landing_page, name='landing page'),
     path('dashboard/', dashboard_page, name='dashboard page'),
+    path('profile/<str:username>/', UserProfileView.as_view(), name='user profile'),
+    path('myprofile/', my_profile, name='my profile'),
+
 ]
--- SocialSnap/Site/views.py
+++ SocialSnap/Site/views.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
+from django.views import View
+from SocialSnap.app_auth.models import User
 
 
 def landing_page(request):
@@ -13,3 +16,28 @@ def dashboard_page(request):
     }
 
     return render(request, context=context, template_name='common/dashboard.html')
+
+
+class UserProfileView(View):
+    template_name = 'common/profile.html'
+
+    def get(self, request, username):
+        user = get_object_or_404(User, username=username)
+
+        context = {
+            'username': user.username,
+            'email': user.email,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+        }
+
+        return render(request, self.template_name, context=context)
+
+
+@login_required
+def my_profile(request):
+    username = request.user.username
+
+    url = reverse('user profile', kwargs={'username': username})
+
+    return redirect(url)
