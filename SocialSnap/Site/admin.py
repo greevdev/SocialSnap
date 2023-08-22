833489564de04c8b7f2729614551f72baba7d2b5
greevdev <greev1@protonmail.com>
2023-08-22 10:53:28 +0300

    Add Comments to Admin Dashboard

--- SocialSnap/Site/admin.py
+++ SocialSnap/Site/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -7,4 +7,10 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ['creator_id', 'created_at']
 
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'content', 'creator_id', 'created_at']
+    list_filter = ['creator_id', 'created_at']
+
+
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
